077dc9936a2309c9efda2fa51396e66bf329a3e1
Branislav Vezilic <banex93@gmail.com>
2021-09-02 00:14:47 +0200

    Updated Content Loss

--- neural_style_transfer/src/losses.py
+++ neural_style_transfer/src/losses.py
@@ -8,11 +8,11 @@ from PIL import Image
 import torchvision.transforms as transforms
 import torchvision.models as models
 
-
 # Pre-trained VGG19
 from torch import Tensor
 
-model=models.vgg19(pretrained=True).features
+model = models.vgg19(pretrained=True).features
+
 
 def get_device():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -20,20 +20,14 @@ def get_device():
 
 
 class TotalLoss(nn.Module):
-    """
-    Requires
-    p - content image
-    a - style image
-    x - generated image
-    """
-    def __init__(self, alpha=1., beta=0.001):
+    def __init__(self, content_features: Tensor, style_features: Tensor, alpha: float = 1., beta: float = 1000.):
         super(TotalLoss, self).__init__()
 
         self.alpha = alpha
         self.beta = beta
 
-        self.content_loss = ContentLoss()
-        self.style_loss = StyleLoss()
+        self.content_loss = ContentLoss(content_features)
+        self.style_loss = StyleLoss(style_features)
 
     def forward(self):
         total_loss = self.alpha * self.content_loss + self.beta * self.style_loss
@@ -41,14 +35,14 @@ class TotalLoss(nn.Module):
 
 
 class ContentLoss(nn.Module):
-    def __init__(self, ):
+    def __init__(self, content_features: Tensor):
         super(ContentLoss, self).__init__()
+
         self.mse = nn.MSELoss()
+        self.content_features = content_features
 
-    def forward(self, content_features: Tensor, generated_features: Tensor) -> Tensor:
-        return self.mse(content_features, generated_features)
-
-
+    def forward(self, input_features: Tensor) -> Tensor:
+        return self.mse(input_features, self.content_features)
 
 
 class StyleLoss(nn.Module):
