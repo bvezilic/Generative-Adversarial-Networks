18e700454123e1835141566b17db66cc479c1154
Branislav Vezilic <banex93@gmail.com>
2021-09-03 00:52:05 +0200

    Added gram matrix

--- neural_style_transfer/src/losses.py
+++ neural_style_transfer/src/losses.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -46,8 +48,30 @@ class ContentLoss(nn.Module):
 
 
 class StyleLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, style_features: List[Tensor]):
         super(StyleLoss, self).__init__()
 
-    def forward(self):
-        pass
+        self.mse = nn.MSELoss()
+        self.style_features = style_features
+        self.style_gram_matrix = torch.cat([gram_matrix(style_feature) for style_feature in self.style_features])
+
+    def forward(self, input_features: List[Tensor]) -> Tensor:
+        assert len(input_features) == len(self.style_gram_matrix), \
+            f"Mismatched lengths of features! {len(input_features)} != {len(self.style_features)}"
+
+        return self.mse
+
+
+
+def gram_matrix(feature_maps: Tensor, normalize: bool = False) -> Tensor:
+    B, C, H, W = feature_maps.size()
+
+    assert B == 1, "Batch size must be 1!"
+
+    features = feature_maps.view(B * C, H * W)
+    g = torch.mm(features, features.t())
+
+    if normalize:
+        g = g / g.numel()
+
+    return g
