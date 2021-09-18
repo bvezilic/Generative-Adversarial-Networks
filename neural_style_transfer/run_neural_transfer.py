eb7ac44e34f3f8abb7f0298f3a8ba16937d71a2b
Branislav Vezilic <banex93@gmail.com>
2021-09-18 12:12:36 +0200

    Added optimizer and print-outs

--- neural_style_transfer/run_neural_transfer.py
+++ neural_style_transfer/run_neural_transfer.py
@@ -1,6 +1,9 @@
+import torch.cuda
 from PIL import Image
-from torchvision.transforms import Compose, Normalize, Resize, ToTensor, Lambda
+from torchvision.transforms import Compose, Normalize, Resize, ToTensor, Lambda, ToPILImage
+import torch.optim as optim
 
+import tqdm
 from src.losses import TotalLoss
 from src.model import VGG19
 from config import IMAGE_DIR
@@ -11,6 +14,7 @@ def run_neural_transfer(
         style_image_path: str,
 ):
     # LOAD IMAGES
+    print("Loading images...")
     images = {
         'content_image': Image.open(content_image_path),
         'style_image': Image.open(style_image_path),
@@ -41,14 +45,52 @@ def run_neural_transfer(
         content_layers=content_layers,
         style_layers=style_layers,
     )
+    print(model)
+
+    # DEVICE SETUP
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device).eval()  # Set immediately to eval mode
+    images = {name: image.to(device) for name, image in images.items()}
 
     # LOSS FUNCTION
     _, content_features, _ = model(images['content_image'])
     _, _, style_features = model(images['style_image'])
-
     loss_criterion = TotalLoss(content_features.values(), style_features.values())
 
-    # Style transfer
+    # OPTIMIZER
+    images['input_image'].requires_grad_(True)
+    model.requires_grad_(False)
+    optimizer = optim.Adam(params=[images['input_image']])
+
+    # FINAL RUN
+    iterations = 10000
+    for i in tqdm.tqdm(range(iterations)):
+        # Forward pass
+        _, content_features, style_features = model(images['input_image'])
+        # Compute loss
+        loss = loss_criterion(
+            input_content_features=content_features.values(),
+            input_style_features=style_features.values()
+        )
+        # Compute gradients
+        loss.backward()
+        # Update input image
+        optimizer.step()
+        # Clear gradients
+        optimizer.zero_grad()
+
+        if i % 50:
+            print(f"Iteration: {i+1}, Loss: {loss.item()}")
+
+    # SAVE INPUT IMAGE
+    postprocessing = Compose([
+        Lambda(lambda x: x.squeeze(0)),  # Removes batch dim
+        Lambda(lambda x: x.clamp(0, 1)),
+        ToPILImage()
+    ])
+
+    final_image = postprocessing(images['input_image'])
+    final_image.save('images/generated.jpg')
 
 
 if __name__ == '__main__':
